94fbc58334cec25e81eaa9dd905aece90da06c87
insomn1aa <76618127+insomn1aa@users.noreply.github.com>
2020-12-27 16:24:06 +0300

    Update 02 how old r u.py

--- 02 how old r u.py
+++ 02 how old r u.py
@@ -1,6 +1,9 @@
 print("hello user")
 user_name = input("whats ur name?       ")
-print("hello" , user_name)
+print("hello" , user_name, "in your name ", len(user_name), 'letters')
 user_year = int(input("enter the year of your birth    "))
-print("nice year,", user_year)
-print(2020 - user_year)
+if len(str(user_year)) == 4:
+    print("nice year,", user_year)
+    print(2020 - user_year)
+else:
+    print("u make mistake")
