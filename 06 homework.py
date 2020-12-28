fc9b9a7ecf06a0bd22838bdd817cd3c6460b14d7
insomn1aa <76618127+insomn1aa@users.noreply.github.com>
2020-12-28 14:48:49 +0300

    Update 06 homework.py

--- 06 homework.py
+++ 06 homework.py
@@ -1,10 +1,10 @@
 guessed = False
 while guessed != True:
     answer = int(input("the number is"))
-    if answer == 40:
+    if answer == 55:
         print("lucky")
         guessed = True
-    elif answer > 40:
+    elif answer > 55:
         print('the number is smaller')
     else:
         print('the number is bigger')
